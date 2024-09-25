6efa84f4436316bc5c9dc95f599e285ccfaa26e7
Anıl EREN <anileren_w@hotmail.com>
2024-09-25 11:08:25 +0300

    Proxy Settings

--- frontendWS/vite.config.js
+++ frontendWS/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //added proxy. and now we don't need a @CrossOrigin anotation in backend 
+  server:{
+    proxy:{
+      '/api': 'http://localhost:8080'
+    }
+  }
 })
--- webService/src/main/java/com/projectX/webService/user/UserController.java
+++ webService/src/main/java/com/projectX/webService/user/UserController.java
@@ -1,10 +1,8 @@
 package com.projectX.webService.user;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class UserController {
     @PostMapping("/api/v1/users")
