48191551179dc5d4af6c644981cb7dd5f85211a9
Anıl EREN <anileren_w@hotmail.com>
2024-09-25 11:41:23 +0300

    saving user to database

---
+++ webService/src/main/java/com/projectX/webService/user/User.java
@@ -0,0 +1,62 @@
+package com.projectX.webService.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //When we use this annotation, it will assume that this class has a matching table in the database, or it will create one.
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue //delegate the operation of generating the ID to the database.
+    long id;
+    
+    String username;
+
+    String email;
+
+    String password;
+
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+
+}
--- webService/src/main/java/com/projectX/webService/user/UserController.java
+++ webService/src/main/java/com/projectX/webService/user/UserController.java
@@ -1,10 +1,18 @@
 package com.projectX.webService.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping("/api/v1/users")
-    void createUser(){}
+    void createUser(@RequestBody User user){
+        userRepository.save(user);
+    }
 }
