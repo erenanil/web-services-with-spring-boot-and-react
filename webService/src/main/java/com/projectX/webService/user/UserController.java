c670dc8a8fb70f722fbe22fa09ba70c6bc2b74b6
Anıl EREN <anileren_w@hotmail.com>
2024-09-26 12:19:28 +0300

    Username Validation

--- webService/src/main/java/com/projectX/webService/user/UserController.java
+++ webService/src/main/java/com/projectX/webService/user/UserController.java
@@ -1,10 +1,15 @@
 package com.projectX.webService.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projectX.webService.error.ApiError;
 import com.projectX.webService.shared.GenericMessage;
 
 @RestController
@@ -16,9 +21,21 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user){
+    ResponseEntity<?> createUser(@RequestBody User user){
+        if (user.getUsername() == null || user.getUsername().isEmpty()){
+            ApiError apiError = new ApiError();
+            apiError.setMessage("validation error");
+            apiError.setPath("/api/v1/users");
+            apiError.setStatus(400);
+            Map<String, String> validationErrors= new HashMap<>();
+            validationErrors.put("username", "Username cannot be null");
+            apiError.setValidationErrors(validationErrors); 
+            return ResponseEntity.badRequest().body(apiError);
+        }
+
+
         userService.save(user);
-        return new GenericMessage("User is created.");
+        return ResponseEntity.ok(new GenericMessage("User is created."));
     }
 
 
