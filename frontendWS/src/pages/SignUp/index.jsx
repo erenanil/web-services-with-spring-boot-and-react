4fbb5dc02d533b7c2a74ea23614a4b7c2107fe27
Anıl EREN <121606956+erenanil@users.noreply.github.com.>
2024-10-03 14:58:24 +0300

    i18n React

--- frontendWS/src/pages/SignUp/index.jsx
+++ frontendWS/src/pages/SignUp/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { signUp } from "./api";
 import { Input } from "./components/Input";
+import { useTranslation } from "react-i18next";
 
 export function SignUp() {
   const [username, setUsername] = useState();
@@ -11,7 +12,7 @@ export function SignUp() {
   const [successMessage, setSuccessMessage] = useState();
   const [errors, setErrors] = useState({});
   const [generalError, setGeneralError]= useState(); 
-  
+  const {t}=useTranslation();
     useEffect(()=> {
       setErrors(function(lastErrors){
         return{
@@ -57,7 +58,7 @@ export function SignUp() {
         {
           setErrors(axiosError.response.data.validationErrors);
         }else{
-          setGeneralError("Unexpected error occured. Please try again.");
+          setGeneralError(t('genericError'));
         }
       }finally{
         setApiProgress(false)
@@ -66,7 +67,7 @@ export function SignUp() {
   
   const passwordRepeatError= useMemo(()=> {
     if (password && password != passwordRepeat){
-      return 'Password Missmatch';
+      return t('passwordMismatch');
     }return '';
   },[password,passwordRepeat]);
       
@@ -75,17 +76,17 @@ export function SignUp() {
       <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
         <form className="card" onSubmit={onSubmit}>
           <div className="text-center card-header">
-            <h1>Sign Up</h1>
+            <h1>{t('signUp')}</h1>
           </div>
           <div className="card-body">
 
-          <Input id="username" label="Username" error={errors.username} onChange = {(event) => setUsername(event.target.value)}/>
+          <Input id="username" label={t('username')} error={errors.username} onChange = {(event) => setUsername(event.target.value)}/>
 
-          <Input id="email" label="E-mail" error={errors.email} onChange= {(event) => setEmail(event.target.value)}/>
+          <Input id="email" label={t('email')} error={errors.email} onChange= {(event) => setEmail(event.target.value)}/>
 
-          <Input id="password" label="Password" error={errors.password} onChange= {(event) => setPassword(event.target.value)} type = "password"/> 
+          <Input id="password" label={t('password')} error={errors.password} onChange= {(event) => setPassword(event.target.value)} type = "password"/> 
 
-          <Input id="passwordRepeat" label="Password Repeat" error={passwordRepeatError} onChange= {(event) => setPasswordRepeat(event.target.value)} type = "password"/>
+          <Input id="passwordRepeat" label={t('passwordRepeat')} error={passwordRepeatError} onChange= {(event) => setPasswordRepeat(event.target.value)} type = "password"/>
           
             {successMessage && (
               <div className="alert alert-success">{successMessage}</div>
@@ -107,7 +108,7 @@ export function SignUp() {
                     aria-hidden="true"
                   ></span>
                 )}
-                Sign Up
+                {t('signUp')}
               </button>
             </div>
           </div>
