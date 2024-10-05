796089db6a7fa199af9c00b0c9d88dcbbc74d9ed
Anıl EREN <121606956+erenanil@users.noreply.github.com.>
2024-10-05 16:42:17 +0300

    MailSender Rollback

--- webService/src/main/java/com/projectX/webService/user/UserService.java
+++ webService/src/main/java/com/projectX/webService/user/UserService.java
@@ -1,12 +1,20 @@
 package com.projectX.webService.user;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.projectX.webService.user.exception.NotUniqueEmailException;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
@@ -16,6 +24,7 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Transactional(rollbackOn = MailException.class)
     public void save(User user) {
         /*
          * String encodedPassowrd = passwordEncoder.encode(user.getPassword());
@@ -26,9 +35,30 @@ public class UserService {
             user.setPassword(passwordEncoder.encode(user.getPassword())); // inline usage.
             user.setActivationToken(java.util.UUID.randomUUID().toString());
             userRepository.save(user);
+            sendActivationEmail(user);
         } catch (DataIntegrityViolationException ex) {
             throw new NotUniqueEmailException();
         }
     }
 
+    private void sendActivationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@my-app.com");
+        message.setTo(user.getEmail());
+        message.setSubject("Email Activation");
+        message.setText("http://localhost:5173/activation/" + user.getActivationToken());
+        getJavaMailSender().send(message);
+    }
+
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.ethereal.email");
+        mailSender.setPort(587);
+        mailSender.setUsername("mable.kiehn2@ethereal.email");
+        mailSender.setPassword("xDE9UewGgathmEZzPx-");
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.smtp.starttls.enable", true);
+        return mailSender;
+    }
 }
