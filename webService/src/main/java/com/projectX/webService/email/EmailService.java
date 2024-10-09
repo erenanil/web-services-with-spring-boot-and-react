f8c10bbbd2cc334c5bb1e7a725c586f695661255
Anıl EREN <121606956+erenanil@users.noreply.github.com.>
2024-10-09 23:18:52 +0300

    localized HTML mail

--- webService/src/main/java/com/projectX/webService/email/EmailService.java
+++ webService/src/main/java/com/projectX/webService/email/EmailService.java
@@ -3,30 +3,31 @@ package com.projectX.webService.email;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.projectX.webService.configuration.ProjectXProperties;
+import com.projectX.webService.shared.Messages;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
-
-
 public class EmailService {
 
-    JavaMailSenderImpl mailSender;
     
+    JavaMailSenderImpl mailSender;
+
     @Autowired
     ProjectXProperties projectXProperties;
     
     @PostConstruct
     public void initialize() {
         this.mailSender = new JavaMailSenderImpl();
-        System.out.println("-----------------------");
-        System.out.println(projectXProperties.getClient());
-        System.out.println("--------------------------");
         mailSender.setHost(projectXProperties.getEmail().host());
         mailSender.setPort(projectXProperties.getEmail().port());
         mailSender.setUsername(projectXProperties.getEmail().username());
@@ -35,17 +36,41 @@ public class EmailService {
         Properties properties = mailSender.getJavaMailProperties();
         properties.put("mail.smtp.starttls.enable", true);
     }
-    
-    public void sendActivationEmail(String email, String activationToken) {
-        var activationUrl = projectXProperties.getClient().host() + "/activation/"+ activationToken;
-        
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(projectXProperties.getEmail().from());
-        message.setTo(email);
-        message.setSubject("Email Activation");
-        message.setText(activationUrl);
-        this.mailSender.send(message);
-    }
 
+    String activationEmail = """
+            <html>
+                <body>
+                    <h1>${title}</h1>
+                    <a href="${url}">${clickHere}</a>
+                </body>
+            </html>
+            """;
+
+    public void sendActivationEmail(String email, String activationToken) {
+        
+        var activationUrl = projectXProperties.getClient().host() + "/activation/" + activationToken;
+        var title=Messages.getMessageForLocale("projectx.mail.user.created.title", LocaleContextHolder.getLocale());
+        var clickHere=Messages.getMessageForLocale("projectx.mail.click.here", LocaleContextHolder.getLocale());
+        
+        var mailBody = activationEmail
+                .replace("${url}", activationUrl)
+                .replace("${title}", title)
+                .replace("${clickHere}", clickHere);
+           
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+
+        try {
+            message.setFrom(projectXProperties.getEmail().from());
+            message.setTo(email);
+            message.setSubject(title);
+            message.setText(mailBody, true);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        this.mailSender.send(mimeMessage);
+    }
 
 }
