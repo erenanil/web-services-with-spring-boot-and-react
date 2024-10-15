273a7452b90dfb3ea3369972b991c2edb6651eaf
Anıl EREN <121606956+erenanil@users.noreply.github.com.>
2024-10-15 15:36:47 +0300

    create users

--- webService/src/main/java/com/projectX/webService/WebServiceApplication.java
+++ webService/src/main/java/com/projectX/webService/WebServiceApplication.java
@@ -1,8 +1,15 @@
 package com.projectX.webService;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.projectX.webService.user.User;
+import com.projectX.webService.user.UserRepository;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WebServiceApplication {
@@ -11,4 +18,22 @@ public class WebServiceApplication {
 		SpringApplication.run(WebServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner userCreator(UserRepository userRepository) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				for (var i = 1; i <= 25; i++) {
+					User user = new User();
+					user.setUsername("user" + i);
+					user.setPassword(passwordEncoder.encode("P4ssword"));
+					user.setEmail("user" + i + "@mail.com");
+					user.setActive(true);
+					userRepository.save(user);
+				}
+			}
+
+		};
+	}
 }
