354325f035705c6d7386ec2859724038c921aa08
Anıl EREN <121606956+erenanil@users.noreply.github.com.>
2024-11-28 19:41:42 +0300

    pagination frontend

--- frontendWS/src/pages/Home/components/UserList.jsx
+++ frontendWS/src/pages/Home/components/UserList.jsx
@@ -1,27 +1,31 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react";
 import { loadUsers } from "./api";
 
-export function UserList(){
-    const [users, setUsers] = useState([])
+export function UserList() {
+  const [userPage, setUserPage] = useState({
+    content: [],
+    last: false,
+    first: false,
+    number: 0,
+  });
 
-    useEffect(() => {
-        async function getUsers() {
-          const response = await loadUsers();
-          setUsers(response.data);
-        }
+  const getUsers = useCallback(async (page) => {
+    const response = await loadUsers(page);
+    setUserPage(response.data);
+  },[])
 
-        getUsers();
-      }, []);
+  useEffect(() => {
+   getUsers();
+  }, []);
 
-    return (
-        <>
-            <div>User List</div>
-            {users.map(user => {
-               return <div>{user.username}</div>
-            })
-
-            }
-
-        </>
-    )
+  return (
+    <>
+      <div>User List</div>
+      {userPage.content.map((user) => {
+        return <div>{user.username}</div>;
+      })}
+      {!userPage.first && <button onClick={() => getUsers(userPage.number-1)}>Previous</button>}
+      {!userPage.last && <button onClick={() => getUsers(userPage.number+1)}>Next</button>}
+    </>
+  );
 }
--- webService/src/main/java/com/projectX/webService/user/UserController.java
+++ webService/src/main/java/com/projectX/webService/user/UserController.java
@@ -1,6 +1,5 @@
 package com.projectX.webService.user;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
